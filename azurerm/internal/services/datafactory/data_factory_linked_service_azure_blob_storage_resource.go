bae56513af5add5f73604e6a776c474f700bb2db
orestis.katsanakis <orestis.katsanakis@infinityworks.com>
2021-08-06 09:53:39 +0100

    formatting

--- azurerm/internal/services/datafactory/data_factory_linked_service_azure_blob_storage_resource.go
+++ azurerm/internal/services/datafactory/data_factory_linked_service_azure_blob_storage_resource.go
@@ -68,26 +68,26 @@ func resourceDataFactoryLinkedServiceAzureBlobStorage() *pluginsdk.Resource {
 				ExactlyOneOf: []string{"connection_string", "sas_uri", "service_endpoint"},
 			},
 
-            "sas_token": {
-                Type:     schema.TypeList,
-                Optional: true,
-                MaxItems: 1,
-                Elem: &schema.Resource{
-                    Schema: map[string]*schema.Schema{
-                        "linked_service_name": {
-                            Type:         schema.TypeString,
-                            Required:     true,
-                            ValidateFunc: validation.StringIsNotEmpty,
-                        },
+			"sas_token": {
+				Type:     schema.TypeList,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"linked_service_name": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validation.StringIsNotEmpty,
+						},
 
-                        "secret_name": {
-                            Type:         schema.TypeString,
-                            Required:     true,
-                            ValidateFunc: validation.StringIsNotEmpty,
-                        },
-                    },
-                },
-            },
+						"secret_name": {
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validation.StringIsNotEmpty,
+						},
+					},
+				},
+			},
 
 			"description": {
 				Type:         pluginsdk.TypeString,
